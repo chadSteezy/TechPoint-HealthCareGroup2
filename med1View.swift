304a969524d78ffa29dd0e3e367d6a00dc987c4e
Evan Burge <86165762+chadSteezy@users.noreply.github.com>
2021-07-08 10:57:24 -0400

    PrescriptView & med1View
    The first medication in prescriptView now redirects to med1View, which is the file that should hold its description and who you are sharing the information with.

---
+++ med1View.swift
@@ -0,0 +1,21 @@
+//
+//  med1View.swift
+//  UnID
+//
+//  Created by Evan on 7/8/21.
+//  This is the first viewablee medicine slot
+
+import SwiftUI
+
+struct med1View: View {
+    var body: some View {
+        Text("refer to wireframe")
+        Text("Prescription detail view")
+    }
+}
+
+struct med1View_Previews: PreviewProvider {
+    static var previews: some View {
+        med1View()
+    }
+}
--- prescriptView.swift
+++ prescriptView.swift
@@ -3,8 +3,7 @@
 //  UnID
 //
 //  Created by Evan on 7/7/21.
-// 
-// THIS IS NOT DONE! ONLY HERE SO I DO NOT LOSE PROGRESS!!!
+//
 
 import SwiftUI
 
@@ -27,37 +26,36 @@ struct prescriptView: View {
                     .padding(.top, -75.0)
                     .padding(.bottom, 20.0)
 
-            //prescription button 1
-            VStack{
-                Button(action: {
-                    print("A pre slot 1")
-                }) {
-                    
-                    Text("Acetaminophen     4/13/21")
+                //prescription button 1
+                //click this one to get page to show who you aree sharing with and what the prescription does
+                VStack{
+                    NavigationLink(destination: med1View()){
+                    Text("Acetaminophen     4/13/21            10")
                         .font(.title3)
                         .fontWeight(.medium)
                         .foregroundColor(Color.black)
                         .multilineTextAlignment(.center)
                         .frame(width: 350.0, height: 45.0)
+                        .offset(x: 0.0) //shift the text over
                         .overlay(
                             RoundedRectangle(cornerRadius: 30)
                                 .stroke(Color.gray, lineWidth: 2.0)
                         )
-                }
+                    }
                 .padding(.top, -55.0)
                 
-                
-                //VACCINATION button 2
+                //prescription button 2
                 Button(action: {
                     print("A pre slot 2")
                 }) {
                     
-                    Text("Amoxicillin     7/12/20")
+                    Text("Amoxicillin              7/12/20            8")
                         .font(.title3)
                         .fontWeight(.medium)
                         .foregroundColor(Color.black)
                         .multilineTextAlignment(.center)
                         .frame(width: 350.0, height: 45.0)
+                        .offset(x: 0.0)
                         .overlay(
                             RoundedRectangle(cornerRadius: 30)
                                 .stroke(Color.gray, lineWidth: 2.0)
@@ -70,21 +68,22 @@ struct prescriptView: View {
                     print("A pre slot 3")
                 }) {
                     
-                    Text("Vyvanse     8/13/21")
+                    Text("Vyvanse                  8/13/21            12")
                         .font(.title3)
                         .fontWeight(.medium)
                         .foregroundColor(Color.black)
                         .multilineTextAlignment(.center)
                         .frame(width: 350.0, height: 45.0)
+                        .offset(x: 4)
                         .overlay(
                             RoundedRectangle(cornerRadius: 30)
                                 .stroke(Color.gray, lineWidth: 2.0)
                         )
                 }
-                .padding(/*@START_MENU_TOKEN@*/.all, 9.0/*@END_MENU_TOKEN@*/)
+                .padding(.all, 4.0)
                 
                 
-                
+                }
                 //INACTIVE prescription button 1
                 
                 VStack {
@@ -94,21 +93,21 @@ struct prescriptView: View {
                         .padding(.trailing, 240.0)
                         .padding(.top, 1.0)
                     
-                    Text("Date Received")
-                        .padding(.leading, 310.0)
-                        .padding(-250.0)
+                    Text("Received  Remaining")
+                        .padding(.leading, 245.0)
+                        .padding(-240.0)
                 }
-                
                 Button(action: {
                     print("I pre slot 1")
                 }) {
                     
-                    Text("Oxycodone     3/5/21")
+                    Text("Oxycodone             3/5/21")
                         .font(.title3)
                         .fontWeight(.medium)
                         .foregroundColor(Color.black)
                         .multilineTextAlignment(.center)
                         .frame(width: 350.0, height: 45.0)
+                        .offset(x: -50.0)
                         .overlay(
                             RoundedRectangle(cornerRadius: 30)
                                 .fill(Color.gray)
@@ -117,7 +116,7 @@ struct prescriptView: View {
                         .opacity(0.45)
                 }
             }
-            }
+            //}
             Spacer()
     }
 }
