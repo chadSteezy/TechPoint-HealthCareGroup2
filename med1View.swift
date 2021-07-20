116d29ff0f268c148ea0c8e57bd4184b9880ec13
Evan Burge <86165762+chadSteezy@users.noreply.github.com>
2021-07-20 19:39:50 -0400

    med1View FIXED
    fixed

--- med1View.swift
+++ med1View.swift
@@ -3,14 +3,67 @@
 //  UnID
 //
 //  Created by Evan on 7/8/21.
-//  This is the first viewablee medicine slot
+//  This is the first viewable medicine slot
 
 import SwiftUI
 
 struct med1View: View {
+    
+    @State var toggleIsOn1: Bool = true
+    @State var toggleIsOn2: Bool = false
+    @State var toggleIsOn3: Bool = false
+    
     var body: some View {
-        Text("refer to wireframe")
-        Text("Prescription detail view")
+        VStack{
+            Text("Acetaminophen")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+                .padding(.top, -30.0)
+                .frame(width: 375.0, height: 50.0)
+                .padding(.trailing, 130)
+            Text("Description:")
+                .padding(.trailing, 280)
+            Divider()
+            VStack(alignment: .center){
+                Text("Here is where the bio of ")
+                Text("this medication would be ")
+                Text("")
+                Text("")
+                Text("")
+            }
+            
+            Divider()
+            Text("Sharing")
+                .fontWeight(.semibold)
+                .font(.largeTitle)
+                .padding(.trailing, 250)
+            
+        }.padding(.top, -50.0)
+        
+        VStack{
+            List{
+                HStack{
+                    Text("Community Health Network")
+                    Toggle(isOn: $toggleIsOn1){
+                    }
+                }
+                HStack{
+                    Text("IUPUI")
+                    Toggle(isOn: $toggleIsOn2){
+                    
+                    }
+                }
+                HStack{
+                    Text("Indianapolis MedCheck")
+                    Toggle(isOn: $toggleIsOn3){
+                    
+                    }
+                
+                }
+            }
+        }
+        .padding(.top, -10)
+        
     }
 }
 
